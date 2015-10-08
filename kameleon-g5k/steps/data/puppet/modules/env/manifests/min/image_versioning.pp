a155f5fed1038f491fe25a5f3954dcef593b5abc
Marc Pinhède <marc.pinhede@inria.fr>
2015-10-08 17:14:47 +0200

    [kameleon-g5k/puppet/min+] Added git tag into generated images
    Change-Id: Ic4cdc0972d614f488d19d6c40fa9b573bea222ae

--- kameleon-g5k/steps/data/puppet/modules/env/manifests/min/image_versioning.pp
+++ kameleon-g5k/steps/data/puppet/modules/env/manifests/min/image_versioning.pp
@@ -8,10 +8,13 @@ class env::min::image_versioning () {
       mode     => '0755',
       owner    => root,
       group    => root;
+    # As a reminder, this file is checked by postinstall to know if br0 must be installed or not.
+    # We will then use it to store the git commit hash of git commit used to generate this image
     "/etc/kaenv/${env::variant}.dsc":
       ensure   => file,
       mode     => '0644',
       owner    => root,
+      source   => 'puppet:///modules/env/min/image_versioning/git_tag',
       group    => root;
   }
 }
