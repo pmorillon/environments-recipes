dd0cb4c3011862fe380563a6401ad0197907e881
Marc Pinhède <marc.pinhede@inria.fr>
2015-12-15 10:10:11 +0100

    [puppet/min+] Removed use of proxy for apt/gem
    Change-Id: Ie1b916873da88a26fcc717882e4fe2afc05d7bc1

--- kameleon-g5k/steps/data/puppet/modules/env/manifests/min/apt.pp
+++ kameleon-g5k/steps/data/puppet/modules/env/manifests/min/apt.pp
@@ -4,14 +4,6 @@ class env::min::apt {
   #
   include 'env::min::apt::keyring'
 
-  # Proxy
-  #
-  if $env::target_g5k {
-    class { 'env::min::apt::proxy':
-      stage  => 'g5k_adjustment';
-    }
-  }
-
   # Grid'5000 repos
   #
   include 'env::min::apt::grid5000'
