4ab23727bd5d651e305e8b6595010aa26d000e94
Simon Delamare <simon.delamare@ens-lyon.fr>
2016-01-15 18:05:47 +0100

    [puppet/all] Restore /etc/grid5000 to store environment information

--- kameleon-g5k/steps/data/puppet/modules/env/manifests/min/image_versioning.pp
+++ kameleon-g5k/steps/data/puppet/modules/env/manifests/min/image_versioning.pp
@@ -3,24 +3,16 @@
 class env::min::image_versioning () {
 
   file {
-    '/etc/kaenv':
+    '/etc/grid5000':
       ensure   => directory,
       mode     => '0755',
       owner    => root,
       group    => root;
-    # As a reminder, this file is checked by postinstall to know if br0 must be installed or not.
-    # We will then use it to store the git commit hash of git commit used to generate this image
-    "/etc/kaenv/${env::variant}.dsc":
+    "/etc/grid5000/release":
       ensure   => file,
       mode     => '0644',
       owner    => root,
-      source   => 'puppet:///modules/env/min/image_versioning/git_tag',
-      group    => root;
-    "/etc/kaenv/postinst":
-      ensure   => file,
-      mode     => '0644',
-      owner    => root,
-      source   => 'puppet:///modules/env/min/image_versioning/postinst',
+      source   => 'puppet:///modules/env/min/image_versioning/release',
       group    => root;
   }
 }
